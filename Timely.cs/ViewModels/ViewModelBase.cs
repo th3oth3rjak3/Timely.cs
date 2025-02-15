edab24919e70d49a17b6a8704b9aa79b4d9a4a97
th3oth3rjak3 <jake.d.hathaway@gmail.com>
2025-02-14 21:05:19 -0800

    Add initial app

---
+++ Timely.cs/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Timely.cs.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Timely.cs/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Timely.cs.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
